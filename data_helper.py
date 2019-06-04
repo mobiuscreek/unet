6a44043204930ddab3d50779c3b90f73f2e9da29
mobiuscreek <vyronc@bgs.ac.uk>
2019-06-04 09:18:54 +0100

    k-fold CV

--- data_helper.py
+++ data_helper.py
@@ -1,68 +1,105 @@
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np 
 import os
-import skimage.io as io
 import skimage.transform as trans
 import matplotlib.pyplot as plt
 from glob import glob
+import pdb
 
+from skimage.color import rgb2gray
 
-
-def adjustData(img,mask,flag_multi_class,num_class):
-    if(flag_multi_class):
-        img = img / 255
-        mask = mask[:,:,:,0] if(len(mask.shape) == 4) else mask[:,:,0]
-        new_mask = np.zeros(mask.shape + (num_class,))
-        for i in range(num_class):
-            new_mask[mask == i,i] = 1
-        new_mask = np.reshape(new_mask,(new_mask.shape[0],new_mask.shape[1]*new_mask.shape[2],new_mask.shape[3])) if flag_multi_class else np.reshape(new_mask,(new_mask.shape[0]*new_mask.shape[1],new_mask.shape[2]))
-        mask = new_mask
-    elif(np.max(img) > 1):
+def adjustData(img,mask):
         img = img / 255
         mask = mask /255
         mask[mask > 0.5] = 1
         mask[mask <= 0.5] = 0
-    return (img,mask)
+        return (img,mask)
 
 
 
-def dataGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "grayscale",
-                    mask_color_mode = "grayscale",image_save_prefix  = "image",mask_save_prefix  = "mask",
-                    flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (256,256),seed = 1):
+def dataGenerator(batch_size,X_train,Y_train,aug_dict,image_save_prefix  = "image", 
+                    save_to_dir = None,seed = 1):
     '''
     can generate image and mask at the same time
     use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
     if you want to visualize the results of generator, set save_to_dir = "your path"
     '''
     image_datagen = ImageDataGenerator(**aug_dict)
-    mask_datagen = ImageDataGenerator(**aug_dict)
-    image_generator = image_datagen.flow_from_directory(
-        train_path,
-        classes = [image_folder],
-        class_mode = None,
-        color_mode = image_color_mode,
-        target_size = target_size,
+    
+    image_generator = image_datagen.flow(
+        X_train, Y_train,
         batch_size = batch_size,
         save_to_dir = save_to_dir,
         save_prefix  = image_save_prefix,
         seed = seed)
-    mask_generator = mask_datagen.flow_from_directory(
-        train_path,
-        classes = [mask_folder],
-        class_mode = None,
-        color_mode = mask_color_mode,
-        target_size = target_size,
-        batch_size = batch_size,
-        save_to_dir = save_to_dir,
-        save_prefix  = mask_save_prefix,
-        seed = seed)
-    train_generator = zip(image_generator, mask_generator)
-    for (img,mask) in train_generator:
-        img,mask = adjustData(img,mask,flag_multi_class,num_class)
+    for (img,mask) in image_generator:
+        img,mask = adjustData(img,mask)
         yield (img,mask)
 
+from sklearn.model_selection import KFold
+from skimage import io
+
+def load_data_Kfold(path_X,path_Y,k):
+    train_files = glob(os.path.join(path_X,'*.jpg'))
+    train_labels = glob(os.path.join(path_Y,'*.jpg'))
+    X_train_np = np.asarray(train_files)
+    Y_train_np = np.asarray(train_labels)
+    kf = KFold(n_splits=k,shuffle=True,random_state=1)
+    X_valid = []
+    X_train = []
+    y_train = []
+    y_valid = []
+    X = np.zeros(shape=(1,1))
+    X_validation = np.zeros(shape=(1,1))
+    Y = np.zeros(shape=(1,1))
+    Y_validation = np.zeros(shape=(1,1))
+    for train_index, test_index in kf.split(X_train_np):
+        X_train.append([np.sort(X_train_np[train_index])])
+        X_valid.append([np.sort(X_train_np[test_index])])
+        y_train.append([np.sort(Y_train_np[train_index])])
+        y_valid.append([np.sort(Y_train_np[test_index])])
+    return X_train, X_valid, y_train, y_valid    
 
 
+
+def get_items(list_of_lists):
+    image_list = [] 
+    flat_list = [item for list_of_lists[0] in list_of_lists for item in list_of_lists[0]]
+    for j in range(len(flat_list)):
+        img = io.imread(flat_list[j],as_gray = True)
+        img = trans.resize(img,(256,256))
+        image_list.append(img)
+        image_np = np.asarray(image_list)
+        image_np = np.expand_dims(image_np,axis=3)
+    return image_np   
+
+
+
+'''
+for train_index, test_index in kf.split(X_train_np):
+        if First_iteration:
+            X_valid.append(X_train_np[test_index])
+            X_train.append(X_train_np[train_index])
+            y_train.append(Y_train_np[train_index])
+            y_valid.append(Y_train_np[test_index])
+            X_valid = np.asarray(X_valid)
+            X_train = np.asarray(X_train)
+            y_train = np.asarray(y_train)
+            y_valid = np.asarray(y_valid)
+            First_iteration = False
+        else:
+          pdb.set_trace()
+          X =  np.vstack((X_train,X_train_np[train_index].reshape(1,X_train_np[train_index].shape[0])))
+          X_validation =  np.vstack((X_valid,X_train_np[test_index].reshape(1,X_train_np[test_index].shape[0])))
+          Y =  np.vstack((y_train,Y_train_np[train_index].reshape(1,Y_train_np[train_index].shape[0])))
+          Y_validation =  np.vstack((y_valid,Y_train_np[test_index].reshape(1,Y_train_np[test_index].shape[0])))
+      #X_train.append(X_train_np[train_index])
+        # np.append(X_valid,X_train_np[test_index])
+        # np.append(y_train,Y_train_np[train_index])
+        # np.append(y_valid,Y_train_np[test_index])
+        #y_train, y_valid = Y_train_np[train_index], Y_train_np[test_index]
+    return X, X_validation, Y, Y_validation
+'''
 def test_file_reader(test_path,target_size = (256,256),as_gray = True):
     '''
         Reads path, resized and returns all images on specified folder
@@ -96,3 +133,6 @@ def plot_metrics(history_obj):
     
 
 
+if __name__ == "__main__":
+    load_data_Kfold('data\\membrane\\train\\image','data\\membrane\\train\\label',4)
+    
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import unet_model
 import data_helper
+import pdb
         
-BATCH_SIZE = 2
+BATCH_SIZE = 16
         
 data_gen_args = dict(rotation_range=0.2,
                     width_shift_range=0.05,
@@ -11,18 +12,30 @@ data_gen_args = dict(rotation_range=0.2,
                     horizontal_flip=True,
                     fill_mode='nearest')
 
-myGene = data_helper.dataGenerator(BATCH_SIZE,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+im_path = 'data/membrane/train/image'
+label_path = 'data/membrane/train/label'
+k = 2
+seed = 1
 
+x_train,x_validation,y_train,y_validation = data_helper.load_data_Kfold(im_path,label_path,k)
+#Training 
 model = unet_model.unet()
-#model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-history = model.fit_generator(myGene,steps_per_epoch=1,epochs=3)
+print(x_train[0][0][0:2])
+print(y_train[0][0][0:2])
+print(x_validation[0][0][0:2])
+print(y_validation[0][0][0:2])
 
-figure = data_helper.plot_metrics(history)
+for fold_number in range(k):
+    x_training = data_helper.get_items(x_train[fold_number])
+    y_training = data_helper.get_items(y_train[fold_number])
+    x_valid = data_helper.get_items(x_validation[fold_number])
+    y_valid = data_helper.get_items(y_validation[fold_number])
+    print(f'Training fold {fold_number}')
+    generator = data_helper.dataGenerator(BATCH_SIZE, x_training,y_training,data_gen_args,seed = 1) 
+    model.fit_generator(generator,steps_per_epoch=len(x_training)/BATCH_SIZE,epochs=1,verbose=1,validation_data = (x_valid,y_valid))
 
-testGene = data_helper.test_file_reader("data/membrane/test")
 
-#validation set
-loss, acc = model.evaluate_generator(myGene, steps=10)
 
-results = model.predict_generator(testGene,10,verbose=1)
-data_helper.saveResult("data/test",results)
+
+#results = model.predict_generator(testGene,10,verbose=1)
+#data_helper.saveResult("data/test",results)
