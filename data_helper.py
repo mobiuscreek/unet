56720275df301d3164c8ec5243b5efc1791d8309
mobiuscreek <vyronc@bgs.ac.uk>
2019-06-07 14:55:43 +0100

    Fix cross validation

--- data_helper.py
+++ data_helper.py
@@ -1,30 +1,16 @@
-from keras.preprocessing.image import ImageDataGenerator
-import numpy as np 
 import os
-import skimage.transform as trans
 import matplotlib.pyplot as plt
+import numpy as np 
+import skimage.transform as trans
+
+from skimage.filters import threshold_otsu
 from glob import glob
-
-from skimage.color import rgb2gray
-
-def adjustData(img,mask):
-        img = img / 255
-        mask = mask /255
-        mask[mask > 0.5] = 1
-        mask[mask <= 0.5] = 0
-        return (img,mask)
-
-def adjustDataTest(img,mask):
-    for i in range(len(img)):
-        img = img /255
-        mask = mask/255
-        mask[mask > 0.5] =1
-        mask[mask <= 0.5] = 0
-        yield (img,mask)
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import KFold
+from skimage import io
 
 
-
-def dataGenerator(batch_size,X_train,Y_train,aug_dict,image_save_prefix  = "image", 
+def dataGenerator(batch_size,im_data ,label_data, aug_dict,image_save_prefix  = "image", 
                     save_to_dir = None,seed = 1):
     '''
     can generate image and mask at the same time
@@ -32,19 +18,27 @@ def dataGenerator(batch_size,X_train,Y_train,aug_dict,image_save_prefix  = "imag
     if you want to visualize the results of generator, set save_to_dir = "your path"
     '''
     image_datagen = ImageDataGenerator(**aug_dict)
-    
+    mask_datagen = ImageDataGenerator(**aug_dict)
     image_generator = image_datagen.flow(
-        X_train, Y_train,
+        im_data, y=None,
         batch_size = batch_size,
         save_to_dir = save_to_dir,
         save_prefix  = image_save_prefix,
         seed = seed)
-    for (img,mask) in image_generator:
-        img,mask = adjustData(img,mask)
-        yield (img,mask)
+    mask_generator = mask_datagen.flow(
+        label_data,y=None,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = image_save_prefix,
+        seed = seed)
+    train_generator = zip(image_generator, mask_generator)
+    for (img,mask) in train_generator:
+            glob_thresh = threshold_otsu(mask)
+            binary_mask = mask > glob_thresh
+            yield (img,binary_mask)       
 
-from sklearn.model_selection import KFold
-from skimage import io
+
+
 
 def load_data_Kfold(path_X,path_Y,k):
     train_files = glob(os.path.join(path_X,'*.jpg'))
@@ -65,12 +59,12 @@ def load_data_Kfold(path_X,path_Y,k):
 
 
 
-def get_items(list_of_lists):
+def get_items(list_of_lists, target_dim = (256,256)):
     image_list = [] 
     flat_list = [item for list_of_lists[0] in list_of_lists for item in list_of_lists[0]]
     for j in range(len(flat_list)):
         img = io.imread(flat_list[j],as_gray = True)
-        img = trans.resize(img,(256,256))
+        img = trans.resize(img, target_dim, mode='constant')
         image_list.append(img)
         image_np = np.asarray(image_list)
         image_np = np.expand_dims(image_np,axis=3)
@@ -78,15 +72,15 @@ def get_items(list_of_lists):
 
 
 
-def test_file_reader(test_path,target_size = (256,256),as_gray = True):
+
+def test_file_reader(test_path, as_gray = True, target_dim = (256,256)):
     '''
         Reads path, resized and returns all images on specified folder
     '''
     extensions = glob(os.path.join(test_path,'*.jpg'))
     for filename in extensions:
         img = io.imread(filename,as_gray = as_gray)
-        img = img / 255
-        img = trans.resize(img,target_size)
+        img = trans.resize(img, target_dim, mode='constant')
         img = np.reshape(img,img.shape+(1,))
         img = np.reshape(img,(1,)+img.shape)
         yield img
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import unet_model
-from data_helper import adjustDataTest, dataGenerator,load_data_Kfold, get_items
+from data_helper import dataGenerator,load_data_Kfold, get_items, test_file_reader, saveResult
+
+
 
 BATCH_SIZE = 2
-
+        
 data_gen_args = dict(rotation_range=0.2,
                     width_shift_range=0.05,
                     height_shift_range=0.05,
@@ -13,27 +15,29 @@ data_gen_args = dict(rotation_range=0.2,
 
 im_path = 'data/membrane/train/image'
 label_path = 'data/membrane/train/label'
+im_test = 'data/membrane/train/im_test'
+
 k = 2
 seed = 1
 
 #Create folds
 x_train,x_validation,y_train,y_validation = load_data_Kfold(im_path,label_path,k)
 
-#Training 
+#Load model
 model = unet_model.unet()
 
+#CV and training
 for fold_number in range(k):
     x_training = get_items(x_train[fold_number])
     y_training = get_items(y_train[fold_number])
     x_valid = get_items(x_validation[fold_number])
     y_valid = get_items(y_validation[fold_number])
-    gen_test = adjustDataTest(x_valid,y_valid)
     print(f'Training fold {fold_number}')
     generator = dataGenerator(BATCH_SIZE, x_training,y_training,data_gen_args,seed = 1) 
-   # model.fit(x_training,y_training,steps_per_epoch=int(len(x_training)/BATCH_SIZE),epochs=1,verbose=1,validation_steps = int(len(x_valid)/BATCH_SIZE),validation_data = (x_valid,y_valid))
-    model.fit_generator(generator, steps_per_epoch = len(x_training) // BATCH_SIZE,epochs=1,verbose=1,validation_steps = len(x_valid)// BATCH_SIZE, validation_data=gen_test)
+    model.fit_generator(generator,steps_per_epoch=len(x_training)/BATCH_SIZE,epochs=3,verbose=1,validation_data = (x_valid,y_valid))
 
+#Read test data and evaluate
+testGen = test_file_reader(im_test)
 
-
-#results = model.predict_generator(testGene,10,verbose=1)
-#data_helper.saveResult("data/test",results)
+results = model.predict_generator(testGen,10,verbose=1)
+saveResult("data/membrane/test",results)
